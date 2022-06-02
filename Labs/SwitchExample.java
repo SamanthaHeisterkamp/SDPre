a5f4fb17c7c31087cc9c2f220270ac7169008e07
Samantha Heisterkamp <samanthapaige912@yahoo.com>
2022-06-01 21:33:01 -0600

    complete SwitchExample

---
+++ Labs/SwitchExample.java
@@ -0,0 +1,27 @@
+public class SwitchExample {
+  public static void main(String[] args) {
+    char grade;
+    grade = 'D';
+
+    switch (grade) {
+      case 'A':
+      System.out.println("Fantastic job!");
+      break;
+      case 'B':
+      System.out.println("Good job!");
+      break;
+      case 'C':
+      System.out.println(" You're doing okay.");
+      break;
+      case 'D':
+      System.out.println("Please schedule a conference.");
+      break;
+      case 'F':
+      System.out.println("Start finishing homework and show up to class.");
+      break;
+      default:
+      System.out.println("Invaild grade.");
+
+    }
+  }
+}
