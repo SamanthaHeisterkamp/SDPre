ffed17c0dae791de3228d5e957db77e4802329c3
Samantha Heisterkamp <samanthapaige912@yahoo.com>
2022-06-03 21:33:07 -0600

    complete Objective8Lab3

---
+++ Labs/Objective8Lab3.java
@@ -0,0 +1,17 @@
+public class Objective8Lab3 {
+  public static void main(String[] args) {
+    int counter = 1;
+
+    for (int H=1; H<=20; H+=1){
+      counter = H;
+
+    if (counter % 2 == 0) {
+      System.out.println(counter + " is even.");
+}
+    else {
+      System.out.println(counter + " is odd.");
+
+      }
+}
+  }
+}
