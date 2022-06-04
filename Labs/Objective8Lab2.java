ddf2eabeba9415eb3f9290de1b204760ad644196
Samantha Heisterkamp <samanthapaige912@yahoo.com>
2022-06-03 21:28:38 -0600

    complete Objective8Lab2

---
+++ Labs/Objective8Lab2.java
@@ -0,0 +1,10 @@
+public class Objective8Lab2 {
+  public static void main(String[] args) {
+    int sum = 0;
+
+    for (int G=1; G<=20; G+=1){
+      sum += G;
+    }
+      System.out.println(sum);
+    }
+  }
